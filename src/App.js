2c96949f01f1fab4a8d965756eb2e26ff6202730
Kalpa Vrikcha <kalpabarbosa@gmail.com>
2020-06-23 07:34:27 -0300

    Adicionando loaders e integração com API
    1. Adicionado loaders para CSS, styles e imagens;
    2. Integração com a API, utilizando axios;
    3. Adicionado o dev-server do webpack para hot-reload do app;
    4. Ainda existe um bug no plugin "transform-runtime" do babel.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    
+    const response = await api.post('projects', {
+      "title": `Novo Projeto ${Date.now()}`,
+      "owner": "undefined"
+    });
+
+    const project = response.data;
+
+    if (project) {
+      setProjects([...projects, project]);
+    }
+  }
+
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Título 5" />
+      
+      <ul>
+        {projects.map((project, index) => <li key={project.id}>{project.title}</li>)}
+      </ul>
+      
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
